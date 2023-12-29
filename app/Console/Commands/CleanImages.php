5e26cef9f6b7f291146b0bc44aca76b81a1f7657
Micah Shackelford <micahdshackelford@gmail.com>
2023-12-29 00:56:16 -0600

    feat: clean images command

---
+++ app/Console/Commands/CleanImages.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Shot;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class CleanImages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shotshare:clean-images';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleanup all images';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Cleanup Database Records
+        Shot::query()->truncate();
+
+        // Cleanup Filesystem
+        Storage::cleanDirectory('uploads');
+    }
+}
