0a6c4cdd28bd9d2ee8caba9aba7dca503e44664a
Micah Shackelford <micahdshackelford@gmail.com>
2024-01-14 15:10:13 -0600

    chore: revert change to features defaults

--- config/features.php
+++ config/features.php
@@ -4,5 +4,5 @@
  * A collection of feature flags to enable/disable select functionality
  */
 return [
-    'reactions' => env("FEATURE_REACTIONS", false),
+    'reactions' => env("FEATURE_REACTIONS", true),
 ];
