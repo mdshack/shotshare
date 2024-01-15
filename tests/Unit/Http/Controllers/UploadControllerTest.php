c2f682e6c2f93e3a81ba8e5f83dffd65afe4079a
Micah Shackelford <micahdshackelford@gmail.com>
2024-01-14 18:06:40 -0600

    chore: test upload

---
+++ tests/Unit/Http/Controllers/UploadControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers;
+
+use App\Models\Shot;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class UploadControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    public function test_it_uploads_shots()
+    {
+        Storage::fake();
+
+        [$parent, $child] = [
+            UploadedFile::fake()->image('fileA.jpg'),
+            UploadedFile::fake()->image('fileB.png'),
+        ];
+
+        $this
+            ->actingAs($this->user)
+            ->post(route('upload'), [
+                'images' => [$parent, $child],
+            ]);
+
+        Storage::assertExists($parentPath = 'uploads/'.$parent->hashName());
+        Storage::assertExists($childPath = 'uploads/'.$child->hashName());
+
+        $parentRecord = Shot::wherePath($parentPath)->first();
+        $childRecord = Shot::wherePath($childPath)
+            ->whereParentShotId($parentRecord->id)
+            ->first();
+
+        $this->assertNotNull($parentRecord, 'it created parent record');
+        $this->assertNotNull($childRecord, 'it created child record');
+    }
+}
