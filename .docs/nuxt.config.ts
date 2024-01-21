7ca969cd4817d16fdbe3e983ce788057c568e7ca
Micah Shackelford <micahdshackelford@gmail.com>
2024-01-21 13:44:36 -0600

    chore (docs): Working build + docs homepage

--- .docs/nuxt.config.ts
+++ .docs/nuxt.config.ts
@@ -2,7 +2,12 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      failOnError: false,
+    },
+  },
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
